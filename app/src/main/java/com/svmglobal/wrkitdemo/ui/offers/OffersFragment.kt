11f2685153e5ca3a3258f43cf6087bd66e4b0a99
Danny <danny.12343@hotmail.co.uk>
2019-10-22 22:49:01 +0100

    Move to fragment transactions

--- app/src/main/java/com/svmglobal/wrkitdemo/ui/offers/OffersFragment.kt
+++ app/src/main/java/com/svmglobal/wrkitdemo/ui/offers/OffersFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,7 +25,7 @@ class OffersFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_offers, container, false)
 
         val viewManager = LinearLayoutManager(this.context)
-        val viewAdapter = OffersAdapter(InternalApiClient.getAllImages())
+        val viewAdapter = OffersAdapter(InternalApiClient.getAllImages(), offersViewModel)
 
         root.findViewById<RecyclerView>(R.id.offers_view).apply {
             setHasFixedSize(true)
