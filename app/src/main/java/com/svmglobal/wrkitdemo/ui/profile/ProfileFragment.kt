88ca805d613a9ae9c5e4a9bfd9c48c08c9b6d39c
Danny Shepherd <daniel.shepherd@svmglobal.com>
2019-10-26 23:36:32 +0100

    Add profile screen

--- app/src/main/java/com/svmglobal/wrkitdemo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/svmglobal/wrkitdemo/ui/profile/ProfileFragment.kt
@@ -4,28 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.svmglobal.wrkitdemo.R
 
-class NotificationsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+        profileViewModel =
+            ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        root.findViewById<Button>(R.id.cash_out).setOnClickListener {
+            val action = ProfileFragmentDirections.actionNavigationAccountToCashOutFragment()
+            it.findNavController().navigate(action)
+        }
+
         return root
     }
 }
