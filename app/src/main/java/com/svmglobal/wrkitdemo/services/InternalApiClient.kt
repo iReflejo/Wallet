c4bb159553cf9f1616598d3bf692c26fdde5f982
Danny Shepherd <daniel.shepherd@svmglobal.com>
2019-10-28 08:31:10 +0000

    Fix cardholder name on details

--- app/src/main/java/com/svmglobal/wrkitdemo/services/InternalApiClient.kt
+++ app/src/main/java/com/svmglobal/wrkitdemo/services/InternalApiClient.kt
@@ -23,7 +23,7 @@ object InternalApiClient {
             Offer(
                 "Tesco",
                 "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Tesco_Logo.svg/800px-Tesco_Logo.svg.png",
-                "https://mobilemarketingmagazine.com/wp-content/uploads/Tesco_store_again.jpg",
+                "https://svmvirtodemo.blob.core.windows.net/wrkit-demo/tesco_store.jpg",
                 "Save 3% on all in-store purchases",
                 "Save 3% on all in-store purchases when you shop between now and christmas",
                 0.5f,
@@ -34,9 +34,9 @@ object InternalApiClient {
             Offer(
                 "Starbucks",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiOvW7N7HpJ6vnZs8vG8aR3jRGTCa4FC6OZ5IR1m_kHvksK7L0",
-                "",
+                "https://svmvirtodemo.blob.core.windows.net/wrkit-demo/starbucks_banner.jpg",
                 "Get 10% cashback on all purchases from our limited time Halloween products",
-                "",
+                "Get 10% cashback on all purchases from our limited time Halloween products",
                 0.8f,
                 instoreUse = true,
                 onlineUse = false,
@@ -45,9 +45,9 @@ object InternalApiClient {
             Offer(
                 "McDonalds",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVy23UkzGE13e9FSSBitLm8vPzaUXco9zNMUeTvVXZb9BrJ8fl",
-                "",
+                "https://svmvirtodemo.blob.core.windows.net/wrkit-demo/mcdonalds_banner.jpg",
                 "5% cashback on all purchases",
-                "",
+                "5% cashback on all purchases",
                 1.2f,
                 instoreUse = true,
                 onlineUse = false,
